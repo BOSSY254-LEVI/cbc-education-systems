e1cb5e7abfffb94045e16ecdb38f150eb57f44c8
John Ominde <ominde@jonzjohn.com>
2026-01-30 16:04:26 -0800

    Add enums for user roles, school levels, and assessments

---
+++ src/types/enums.ts
@@ -0,0 +1,53 @@
+// CBC/CBE Platform Enums
+
+export enum UserRole {
+  SUPER_ADMIN = 'super_admin',
+  SCHOOL_ADMIN = 'school_admin',
+  TEACHER = 'teacher',
+  PARENT = 'parent',
+  STUDENT = 'student',
+}
+
+export enum SchoolLevel {
+  ECDE = 'ecde',
+  PRIMARY = 'primary',
+  JUNIOR_SECONDARY = 'junior_secondary',
+  SENIOR_SECONDARY = 'senior_secondary',
+}
+
+export enum GradeLevel {
+  PP1 = 'PP1',
+  PP2 = 'PP2',
+  GRADE_1 = 'Grade 1',
+  GRADE_2 = 'Grade 2',
+  GRADE_3 = 'Grade 3',
+  GRADE_4 = 'Grade 4',
+  GRADE_5 = 'Grade 5',
+  GRADE_6 = 'Grade 6',
+  GRADE_7 = 'Grade 7',
+  GRADE_8 = 'Grade 8',
+  GRADE_9 = 'Grade 9',
+}
+
+export enum AssessmentType {
+  FORMATIVE = 'formative',
+  SUMMATIVE = 'summative',
+}
+
+export enum PerformanceLevel {
+  EXCEEDING_EXPECTATION = 'exceeding_expectation',
+  MEETING_EXPECTATION = 'meeting_expectation',
+  APPROACHING_EXPECTATION = 'approaching_expectation',
+  BELOW_EXPECTATION = 'below_expectation',
+}
+
+export enum Gender {
+  MALE = 'male',
+  FEMALE = 'female',
+}
+
+export enum Term {
+  TERM_1 = 'term_1',
+  TERM_2 = 'term_2',
+  TERM_3 = 'term_3',
+}
